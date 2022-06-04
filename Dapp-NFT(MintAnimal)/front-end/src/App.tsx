64d799bdf2f82c2770217252e3a8f855f1b7a453
wavescats <wavescats@gmail.com>
2022-06-05 03:12:59 +0900

    Dapp-NFT_MintAnimalToken_Front-End-Setting

---
+++ Dapp-NFT(MintAnimal)/front-end/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./routes/main";
+
+const App: FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ Dapp-NFT(MintAnimal)/front-end/src/routes/main.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import { Box } from "@chakra-ui/react";
+
+const Main: FC = () => {
+  return <Box>Main</Box>;
+};
+
+export default Main;
